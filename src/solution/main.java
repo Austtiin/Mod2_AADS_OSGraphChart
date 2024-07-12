ae7a4503a0e09792ea519fd6efadfc6775e9286c
Austtiin <austinstephens103@gmail.com>
2024-07-11 22:12:48 -0500

    Impl
    Impl

--- src/solution/main.java
+++ src/solution/main.java
@@ -1,54 +1,32 @@
 /*
  * main.java
- * Contains the main class for the solution package
+ * Class to run the solution
+ * Solution will be separated into multiple classes for better organization
  * 
  * Created by: Austin Stephens
  * Created on: 07/10/2024
+ * Rasumssen University
+ * Professor: Kumar
  * 
  * Instructions: To handle the modules in your operating system, you will need to implement a stack. 
- * The stack will be for holding the modules in the order in which they have to be compiled. 
- * You may use any data structure, singly-linked list, etc. 
+ * The stack will be for holding the modules in the order in which they have to be compiled.
+ * You may use any data structure, singly-linked list, etc. You may NOT use the Java Stack Class. 
  * However, your program must be able to handle the three stack functions; push, pop, and peek. 
+ * 
  * The final program should be able to take a list of data items and generate a stack. 
- * You are free to specify the data type of the items. However, keep in mind that these items will represent your operating system modules. 
- * You can choose to have the data type as strings 
+ * You are free to specify the data type of the items. 
+ * However, keep in mind that these items will represent your operating system modules
+ * You can choose to have the data type as strings
  * (Module A, Module B, etc. …), characters (A, B, C…) or integers (1, 2,3…).
  * 
  */
 
 package solution;
+import java.util.ArrayList;
+
 
 public class main {
-	public static void main(String[] args) {
-		// Create a new stack
-		Stack stack = new Stack();
-
-		// Push some modules onto the stack
-		stack.push("Module A");
-		stack.push("Module B");
-		stack.push("Module C");
-		stack.push("Module D");
-		stack.push("Module E");
-
-		
-		System.out.println("Top of the stack: " + stack.peek());
-		System.out.println("Popped: " + stack.pop());
-		System.out.println("Top of the stack: " + stack.peek());
-
-		System.out.println("Popped: " + stack.pop());
-
-		System.out.println("Top of the stack: " + stack.peek());
-
-		System.out.println("Popped: " + stack.pop());
-
-		System.out.println("Top of the stack: " + stack.peek());
-
-		System.out.println("Popped: " + stack.pop());
-
-		System.out.println("Top of the stack: " + stack.peek());
-		System.out.println("Popped: " + stack.pop());
-		System.out.println("Top of the stack: " + stack.peek());
-	}
 	
-
+	
+	
 }
---
+++ src/solution/moduleFunc.java
@@ -0,0 +1,43 @@
+/*
+ * moduleFunc.java
+ * Class to handle the stack functions
+ * 
+ * Created by: Austin Stephens
+ * Created on: 07/10/2024
+ * Rasumssen University
+ * Professor: Kumar
+ * 
+ */
+
+package solution;
+
+import java.util.ArrayList;
+
+public class moduleFunc {
+	ArrayList<String> stack = new ArrayList<String>();
+	
+	//Push function
+	public void push(String module) {
+		stack.add(module);
+	}
+	
+	//Pop function
+	public void pop() {
+		if (stack.size() == 0) {
+			System.out.println("Stack is empty");
+		} else {
+			stack.remove(stack.size() - 1);
+		}
+	}
+	
+	//Peek function
+	public void peek() {
+		if (stack.size() == 0) {
+			System.out.println("Stack is empty");
+		} else {
+			System.out.println(stack.get(stack.size() - 1));
+		}
+	}
+	
+	
+}
